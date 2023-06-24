247c54d126f01996c422f7aaeb27c09cbb1d4232
Afif Syaifullah <afifsyaifullah1512@gmail.com>
2023-06-24 22:10:05 +0700

    test

--- components/Moleculs/Home/Home.js
+++ components/Moleculs/Home/Home.js
@@ -52,95 +52,95 @@ const ArrowRightIcon = (props) => {
     </>
   );
 };
-const Resume = () => {
-  let resume = [
-    {
-      company: "Dicoding Indonesia",
-      title: "Belajar Pemrograman Kotlin",
-      logo: logoStartcode,
-      start: "2021",
-      end: {
-        label: "Present",
-        dateTime: new Date().getFullYear(),
-      },
-    },
-    {
-      company: "Redhunter Academy",
-      title: "Belajar Membuat Aplikasi Android Pemula",
-      logo: logoRedhunter,
-      start: "2022",
-      end: {
-        label: "Present",
-        dateTime: new Date().getFullYear(),
-      },
-    },
-    {
-      company: "PT Prantara",
-      title: "Front End Developer",
-      logo: logoPrantara,
-      start: "2021",
-      end: {
-        label: "Present",
-        dateTime: new Date().getFullYear(),
-      },
-    },
-    {
-      company: "PT Kreasi Asia",
-      title: "Front End Developer",
-      logo: logoKreasiAsia,
-      start: "2021",
-      end: "2021 November",
-    },
-  ];
+// const Resume = () => {
+//   let resume = [
+//     {
+//       company: "Dicoding Indonesia",
+//       title: "Belajar Pemrograman Kotlin",
+//       logo: logoStartcode,
+//       start: "2021",
+//       end: {
+//         label: "Present",
+//         dateTime: new Date().getFullYear(),
+//       },
+//     },
+//     {
+//       company: "Redhunter Academy",
+//       title: "Belajar Membuat Aplikasi Android Pemula",
+//       logo: logoRedhunter,
+//       start: "2022",
+//       end: {
+//         label: "Present",
+//         dateTime: new Date().getFullYear(),
+//       },
+//     },
+//     {
+//       company: "PT Prantara",
+//       title: "Front End Developer",
+//       logo: logoPrantara,
+//       start: "2021",
+//       end: {
+//         label: "Present",
+//         dateTime: new Date().getFullYear(),
+//       },
+//     },
+//     {
+//       company: "PT Kreasi Asia",
+//       title: "Front End Developer",
+//       logo: logoKreasiAsia,
+//       start: "2021",
+//       end: "2021 November",
+//     },
+//   ];
 
-  return (
-    <div className="rounded-2xl border bg-white dark:bg-woodsmoke-700 dark:bg-opacity-40 dark:backdrop-blur-lg bg-opacity-60 backdrop-blur-lg border-zinc-100 p-6 dark:border-zinc-700/40">
-      <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
-        <BriefcaseIcon className="h-6 w-6 flex-none" />
-        <span className="ml-3">Certificate Training</span>
-      </h2>
-      <ol className="mt-6 space-y-4">
-        {resume.map((role, roleIndex) => (
-          <li key={roleIndex} className="flex gap-4">
-            <div className="relative mt-1 flex h-10 w-10 p-2 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-              <Image
-                src={role.logo}
-                alt=""
-                className="h-7 w-7  object-contain"
-                unoptimized
-              />
-            </div>
-            <dl className="flex flex-auto flex-wrap gap-x-2">
-              <dt className="sr-only">Company</dt>
-              <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">
-                {role.company}
-              </dd>
-              <dt className="sr-only">Role</dt>
-              <dd className="text-xs text-zinc-500 dark:text-zinc-400">
-                {role.title}
-              </dd>
-              <dt className="sr-only">Date</dt>
-              <dd
-                className="ml-auto text-xs text-zinc-400 dark:text-zinc-500"
-                aria-label={`${role.start.label ?? role.start} until ${
-                  role.end.label ?? role.end
-                }`}
-              >
-                <time dateTime={role.start.dateTime ?? role.start}>
-                  {role.start.label ?? role.start}
-                </time>{" "}
-                <span aria-hidden="true">—</span>{" "}
-                <time dateTime={role.end.dateTime ?? role.end}>
-                  {role.end.label ?? role.end}
-                </time>
-              </dd>
-            </dl>
-          </li>
-        ))}
-      </ol>
-    </div>
-  );
-};
+//   return (
+//     <div className="rounded-2xl border bg-white dark:bg-woodsmoke-700 dark:bg-opacity-40 dark:backdrop-blur-lg bg-opacity-60 backdrop-blur-lg border-zinc-100 p-6 dark:border-zinc-700/40">
+//       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+//         <BriefcaseIcon className="h-6 w-6 flex-none" />
+//         <span className="ml-3">Certificate Training</span>
+//       </h2>
+//       <ol className="mt-6 space-y-4">
+//         {resume.map((role, roleIndex) => (
+//           <li key={roleIndex} className="flex gap-4">
+//             <div className="relative mt-1 flex h-10 w-10 p-2 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+//               <Image
+//                 src={role.logo}
+//                 alt=""
+//                 className="h-7 w-7  object-contain"
+//                 unoptimized
+//               />
+//             </div>
+//             <dl className="flex flex-auto flex-wrap gap-x-2">
+//               <dt className="sr-only">Company</dt>
+//               <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">
+//                 {role.company}
+//               </dd>
+//               <dt className="sr-only">Role</dt>
+//               <dd className="text-xs text-zinc-500 dark:text-zinc-400">
+//                 {role.title}
+//               </dd>
+//               <dt className="sr-only">Date</dt>
+//               <dd
+//                 className="ml-auto text-xs text-zinc-400 dark:text-zinc-500"
+//                 aria-label={`${role.start.label ?? role.start} until ${
+//                   role.end.label ?? role.end
+//                 }`}
+//               >
+//                 <time dateTime={role.start.dateTime ?? role.start}>
+//                   {role.start.label ?? role.start}
+//                 </time>{" "}
+//                 <span aria-hidden="true">—</span>{" "}
+//                 <time dateTime={role.end.dateTime ?? role.end}>
+//                   {role.end.label ?? role.end}
+//                 </time>
+//               </dd>
+//             </dl>
+//           </li>
+//         ))}
+//       </ol>
+//     </div>
+//   );
+// };
 const Project = () => {
   let listProject = [
     {
