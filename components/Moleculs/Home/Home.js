1cbf3a3a5600ffcaf290c5cdec935c4a61fe8519
Afif Syaifullah <afifsyaifullah1512@gmail.com>
2023-06-25 16:56:23 +0700

    test

--- components/Moleculs/Home/Home.js
+++ components/Moleculs/Home/Home.js
@@ -174,6 +174,11 @@ const Project = () => {
       <h1 className="text-2xl font-bold text-woodsmoke-900 dark:text-white">
         The Last Of Project
       </h1>
+      return (
+    <>
+      <h1 className="text-2xl font-bold text-woodsmoke-900 dark:text-white">
+        The Last Of Project
+      </h1>
       <div className="flex flex-col gap-y-6 mt-6">
         {listProject.map((project, index) => {
           return (
@@ -203,6 +208,39 @@ const Project = () => {
           );
         })}
       </div>
+
+      <div className="flex flex-col gap-y-6 mt-6">
+        {listProject.map((project, index) => {
+          return (
+            <div
+              key={index}
+              className="rounded-2xl p-6 hover:bg-zinc-100 dark:bg-woodsmoke-700 dark:bg-opacity-40 dark:backdrop-blur-lg dark:hover:bg-woodsmoke-600 dark:hover:bg-opacity-40 dark:transition-all transition-all"
+            >
+              <span className="border-l-2 border-zinc-200 pl-2 text-woodsmoke-400 text-sm dark:text-woodsmoke-300">
+                {project.date}
+              </span>
+              <h1 className="mt-4 font-semibold text-lg text-woodsmoke-800 dark:text-white">
+                {project.title}
+              </h1>
+              <p className="mt-2 text-woodsmoke-400 text-sm dark:text-woodsmoke-300">
+                {project.desc}
+              </p>
+              <a
+                href={project.link}
+                className={
+                  "flex items-center gap-x-2 text-xs mt-4 w-fit text-woodsmoke-500 hover:text-indigo-500 font-semibold group transition-colors"
+                }
+              >
+                View Project
+                <ArrowRightIcon className="fill-woodsmoke-400 h-4 w-4 group-hover:fill-indigo-500 transition-colors" />
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    </>
+);
+
     </>
   );
 };
@@ -238,9 +276,9 @@ const Home = () => {
         </p>
       </div>
       <div className="mt-16 grid grid-cols-1 lg:grid-cols-7 gap-x-8 gap-y-6">
-        {/* <div className="col-span-1 lg:col-span-4">
+        <div className="col-span-1 lg:col-span-4">
           <Project />
-        </div> */}
+        </div>
         {/* <div className="flex flex-col gap-y-6 col-span-1 lg:col-span-3">
           <Resume />
         </div> */}
