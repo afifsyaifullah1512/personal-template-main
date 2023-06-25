be387e54d14e861848cf9a8738411a766a5474ac
Afif Syaifullah <afifsyaifullah1512@gmail.com>
2023-06-25 17:06:31 +0700

    test

--- components/Moleculs/Home/Home.js
+++ components/Moleculs/Home/Home.js
@@ -74,12 +74,12 @@ const ArrowRightIcon = (props) => {
         <h1 className="text-2xl font-bold text-woodsmoke-900 dark:text-white">
           The Last Of Project
         </h1>
-        <div className="flex flex-wrap gap-6 mt-6">
+        <div className="grid grid-cols-2 gap-6 mt-6">
           {listProject.map((project, index) => {
             return (
               <div
                 key={index}
-                className="flex flex-col rounded-2xl p-6 hover:bg-zinc-100 dark:bg-woodsmoke-700 dark:bg-opacity-40 dark:backdrop-blur-lg dark:hover:bg-woodsmoke-600 dark:hover:bg-opacity-40 dark:transition-all transition-all"
+                className="rounded-2xl p-6 hover:bg-zinc-100 dark:bg-woodsmoke-700 dark:bg-opacity-40 dark:backdrop-blur-lg dark:hover:bg-woodsmoke-600 dark:hover:bg-opacity-40 dark:transition-all transition-all"
               >
                 <span className="border-l-2 border-zinc-200 pl-2 text-woodsmoke-400 text-sm dark:text-woodsmoke-300">
                   {project.date}
@@ -106,6 +106,7 @@ const ArrowRightIcon = (props) => {
       </>
     );
   };
+  
   
 const Home = () => {
   return (
