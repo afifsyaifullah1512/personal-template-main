16c0ac134882df60e0bfba06cfb4545352db23de
Afif Syaifullah <afifsyaifullah1512@gmail.com>
2023-06-25 15:23:30 +0700

    test github

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+
 export function Footer() {
   return (
     <footer className="mt-16">
       <div className="border-t border-zinc-300 pt-10 pb-16 dark:border-zinc-600/40">
         <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
-          <div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200"></div>
+          <div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
+            <a href="https://github.com/your-username" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faGithub} size="lg" />
+            </a>
+          </div>
           <p className="text-sm text-zinc-400 dark:text-zinc-500">
             Afif Syaifullah. All rights reserved.
           </p>
@@ -12,3 +20,4 @@ export function Footer() {
     </footer>
   );
 }
+
