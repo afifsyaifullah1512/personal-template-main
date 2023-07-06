e0398932cfef0979445732c4bdabe74070968f09
Afif Syaifullah <afifsyaifullah1512@gmail.com>
2023-07-06 21:01:09 +0700

    p

--- components/Moleculs/Home/Home.js
+++ components/Moleculs/Home/Home.js
@@ -152,7 +152,7 @@ const Project = () => {
     {
       title: "Coming Soon..",
       desc: "",
-      link: "https://demo.afif-syaifullah.my.id",
+      link: "#",
       date: "Coming Soon",
     },
     // {
